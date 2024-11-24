b8f56e2d19343f73f4d60c4ae06f64dca61502ba
Torsketryne <nikoaa@ntnu.no>
2024-11-24 17:20:52 +0100

    Initalized a branch with the template code

---
+++ src/main/java/idata2304/group13/listeners/greenhouse/SensorListener.java
@@ -0,0 +1,19 @@
+package idata2304.group13.listeners.greenhouse;
+
+import java.util.List;
+import idata2304.group13.greenhouse.Sensor;
+
+/**
+ * Listener for sensor update events.
+ * This will (probably) be usable only on the sensor/actuator node (greenhouse) side, where the
+ * real sensor objects are available. The control panel side has only sensor reading values
+ * available, not the sensors themselves.
+ */
+public interface SensorListener {
+  /**
+   * An event that is fired every time sensor values are updated.
+   *
+   * @param sensors A list of sensors having new values (readings)
+   */
+  void sensorsUpdated(List<Sensor> sensors);
+}
