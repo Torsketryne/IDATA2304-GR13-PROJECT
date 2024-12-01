65b8d90a2b1e49332f46d922d9ea4f7aa7eea710
Daniel Alnes <d.s.alnes93@gmail.com>
2024-12-01 01:37:41 +0100

    Added more tools to handle Messages

---
+++ src/main/java/idata2304/group13/tools/MessageHandler.java
@@ -0,0 +1,32 @@
+package idata2304.group13.tools;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Handle messages.
+ */
+public class MessageHandler {
+
+    /**
+     * Parse a message from a string into a map of key-value pairs.
+     *
+     * @param message The message to parse
+     * @return A map of key-value pairs
+     */
+    public static Map<String, String> parseMessage(String message) {
+        Map<String, String> parsedData = new HashMap<>();
+        String[] data = message.split(";");
+
+        // Split the data into key-value pairs and put them into a map
+        for (String s : data) {
+            String[] keyValue = s.split(":");
+            if (keyValue.length == 2) {
+                parsedData.put(keyValue[0], keyValue[1]);
+            }
+        }
+        return parsedData;
+    }
+
+
+}
